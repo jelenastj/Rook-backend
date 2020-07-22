b2009794536c149aec92becb3e0939cb4f0f4221
jelenastj <jelena.stjepanovic@gmail.com>
2020-07-22 15:24:49 -0500

    tripgears controller updated

--- app/controllers/api/v1/tripgears_controller.rb
+++ app/controllers/api/v1/tripgears_controller.rb
@@ -1,12 +1,38 @@
 class Api::V1::TripgearsController < ApplicationController
     skip_before_action :authorized
+    before_action :find_trip, only: [:show, :update, :destroy]
+
+
     def index
         tripgears = Tripgear.all
         render json: tripgears, except: [:created_at, :updated_at]
     end
 
+
+    def create
+        tripgear = Tripgear.create(tripgear_params)
+        if (tripgear.valid?)
+            render json: tripgear.to_json(tripsgear_serializer)
+        else
+            render json: { errors: tripgear.errors.full_messages }, status: :not_acceptable
+        end
+    end
+
+
     def show
         render json: @tripgear, except: [:created_at, :updated_at]
     end
+
+
+    private
+
+    def find_tripgear
+       @tripgear = Tripgear.find(params[:id])
+    end
+
+    def tripgear_params
+        params.require(:tripgear).permit(:trip_id, :gear_id)
+    end
+
 
 end
